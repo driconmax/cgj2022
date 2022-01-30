54882da695e32b6ca8a4f58d4103256fa54983f3
deboratheaux <79940013+deboratheaux@users.noreply.github.com>
2022-01-30 15:18:17 -0300

    bugueadisimo el menu

--- Assets/Scripts/UnityDelivery/MenuView.cs
+++ Assets/Scripts/UnityDelivery/MenuView.cs
@@ -2,6 +2,7 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
 using System;
+using Spine.Unity;
 
 public class MenuView : MonoBehaviour, Menu
 {
@@ -9,11 +10,50 @@ public class MenuView : MonoBehaviour, Menu
     [SerializeField] TMP_InputField nickname;
     [SerializeField] GameObject lobby;
     [SerializeField] GameObject waitingRoom;
+    [SerializeField] Button next;
+    [SerializeField] Button previous;
+    [SerializeField] SkeletonGraphic animator;
+
+    public int index = 0;
+
+    private void Awake()
+    {
+        ShowLobby();
+    }
 
     public void ShowLobby()
     {
+        var charactersCount = animator.Skeleton.Data.Skins.Count;
+
         //play keyboard sound
         //nickname.onValueChanged.AddListener();
+        play.onClick.AddListener(() => animator.SkeletonData.FindAnimation("menu_SELECTION"));
+       
+        next.onClick.AddListener(() =>
+        {
+            index++;
+
+            if (index >= charactersCount)
+            {
+                index = 0;
+            }
+
+            animator.Skeleton.SetSkin(animator.Skeleton.Data.Skins.Items[index]);
+        });
+
+        previous.onClick.AddListener(() =>
+        {
+            index--;
+
+            if (index < 0)
+            {
+                index = charactersCount - 1;
+            }
+
+            animator.Skeleton.SetSkin(animator.Skeleton.Data.Skins.Items[index]);
+        });
+
+
         lobby.SetActive(true);
     }
 
@@ -23,7 +63,7 @@ public class MenuView : MonoBehaviour, Menu
     }
 
     public void SetUpButtonPlay(Action<string> EnterGame)
-    {
+    { 
         play.onClick.AddListener(() => EnterGame(nickname.text));
     }
 
